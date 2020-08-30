37b9f3696669990e52d43e12d62972772acdea75
shiryauwu <70473372+shiryauwu@users.noreply.github.com>
2020-08-30 15:08:23 +0300

    Add files via upload

---
+++ PEfE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PEfE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.StartMenu();
+        }
+    }
+}
